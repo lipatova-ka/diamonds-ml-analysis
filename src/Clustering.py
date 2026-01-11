8d2a4207058481017a0b8ce5935acb4c5ca19fee
lipatova-ka <kristina.lipatova15@gmail.com>
2026-01-11 11:55:02 +0300

    Update Clustering.py

--- src/Clustering.py
+++ src/Clustering.py
@@ -5,6 +5,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import AgglomerativeClustering
 
 df = pd.read_csv("diamonds_clean_final.csv")
 
@@ -24,6 +28,9 @@ print("Размер данных для кластеризации:", X_cluster.
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X_cluster)
 
+
+
+# K-means
 # Первая проверка — метод локтя (Elbow Method)
 inertia = []
 K = range(2, 11)
@@ -40,7 +47,6 @@ plt.ylabel('Inertia')
 plt.title('Определение оптимального числа кластеров методом локтя')
 plt.grid(True)
 plt.show()
-
 
 # Построение модели K-means с оптимальным числом кластеров
 kmeans = KMeans(n_clusters=4, random_state=42)
@@ -62,11 +68,8 @@ cluster_centers = pd.DataFrame(
 cluster_centers['cluster'] = cluster_centers.index
 print(cluster_centers.to_string(index=False))
 
-
 # Оценка качества кластеризации
-
 # Silhouette Score
-
 sil_score = silhouette_score(X_scaled, clusters)
 print("Silhouette score:", round(sil_score, 3))
 
@@ -79,14 +82,8 @@ print("Calinski-Harabasz Index:", round(ch, 1))
 
 
 # DBSCAN
-from sklearn.cluster import DBSCAN
-
-
 # Подбор оптимального eps
-from sklearn.neighbors import NearestNeighbors
-
-# число ближайших соседей
-k = 10
+k = 10 # число ближайших соседей
 
 neighbors = NearestNeighbors(n_neighbors=k)
 neighbors_fit = neighbors.fit(X_scaled)
@@ -104,6 +101,7 @@ plt.title('k-distance plot для подбора eps')
 plt.grid(True)
 plt.show()
 
+# Выбираем оптимальное значение eps
 eps_list = [0.6, 0.65, 0.7, 0.75, 0.8, 0.85]
 
 for eps in eps_list:
@@ -114,7 +112,7 @@ for eps in eps_list:
     noise_share = n_noise / len(labels)
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
 
-    # silhouette только по не-шуму
+    # silhouette без шума
     from sklearn.metrics import silhouette_score
     mask = labels != -1
     sil = None
@@ -122,7 +120,6 @@ for eps in eps_list:
         sil = silhouette_score(X_scaled[mask], labels[mask])
 
     print(f"eps={eps:.2f} | clusters={n_clusters} | noise={noise_share:.3f} | sil={None if sil is None else round(sil,3)}")
-
 
 dbscan = DBSCAN(
     eps=0.75,      # подобрали по k-distance plot
@@ -137,7 +134,6 @@ X_cluster_dbscan['cluster_dbscan'] = clusters_dbscan
 
 print(X_cluster_dbscan['cluster_dbscan'].value_counts())
 
-
 # Считаем silhouette_score без шума
 mask = clusters_dbscan != -1
 
@@ -150,12 +146,12 @@ if len(set(clusters_dbscan[mask])) > 1:
 else:
     print("Недостаточно кластеров для оценки")
 
-
 df_clustered = X_cluster.copy()
 df_clustered['cluster_dbscan'] = clusters_dbscan
 
 # средние значения признаков по кластерам DBSCAN
 print(df_clustered.groupby('cluster_dbscan')[cluster_features].mean().round(3).to_string())
+
 
 
 # Пробуем метод HDBSCAN
@@ -190,56 +186,51 @@ else:
 noise_share = (labels_hdb == -1).sum() / len(labels_hdb)
 print("Доля шума:", round(noise_share, 3))
 
-# # Иерархическая кластеризация
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.cluster import AgglomerativeClustering
-# from sklearn.metrics import silhouette_score
-#
-# X = df[cluster_features].dropna()
-# Xs = StandardScaler().fit_transform(X)
-#
-# # # Подбор числа кластеров k
-# # for k in range(2, 8):
-# #     model = AgglomerativeClustering(
-# #         n_clusters=k,
-# #         linkage='ward'
-# #     )
-# #     labels = model.fit_predict(Xs)
-# #
-# #     sil = silhouette_score(Xs, labels)
-# #     print(f"k = {k}, Silhouette = {sil:.3f}")
-#
-#
-# agg = AgglomerativeClustering(
-#     n_clusters=4,
-#     linkage='ward'
-# )
-#
-# clusters_agg = agg.fit_predict(Xs)
-#
-# X_cluster_agg = X.copy()
-# X_cluster_agg['cluster_agg'] = clusters_agg
-#
-# # Кол-во значений по кластерам
-# counts = X_cluster_agg['cluster_agg'].value_counts()
-# percentages = counts / counts.sum() * 100
-#
-# cluster_stats = pd.DataFrame({
-#     'count': counts,
-#     'percent': percentages.round(2)
-# })
-#
-# print(cluster_stats)
-#
-#
-#
-#
-# cluster_profile_agg = (
-#     X_cluster_agg
-#     .groupby('cluster_agg')[cluster_features]
-#     .mean()
-#     .round(3)
-# )
-#
-# print(cluster_profile_agg.to_string())
+
+
+# Иерархическая кластеризация
+X = df[cluster_features].dropna()
+Xs = StandardScaler().fit_transform(X)
+
+# Подбор числа кластеров k
+for k in range(2, 8):
+    model = AgglomerativeClustering(
+        n_clusters=k,
+        linkage='ward'
+    )
+    labels = model.fit_predict(Xs)
+
+    sil = silhouette_score(Xs, labels)
+    print(f"k = {k}, Silhouette = {sil:.3f}")
+
+agg = AgglomerativeClustering(
+    n_clusters=4,
+    linkage='ward'
+)
+
+clusters_agg = agg.fit_predict(Xs)
+
+X_cluster_agg = X.copy()
+X_cluster_agg['cluster_agg'] = clusters_agg
+
+# Кол-во значений по кластерам
+counts = X_cluster_agg['cluster_agg'].value_counts()
+percentages = counts / counts.sum() * 100
+
+cluster_stats = pd.DataFrame({
+    'count': counts,
+    'percent': percentages.round(2)
+})
+
+print(cluster_stats)
+
+# средние значения признаков по кластерам
+cluster_profile_agg = (
+    X_cluster_agg
+    .groupby('cluster_agg')[cluster_features]
+    .mean()
+    .round(3)
+)
+
+print(cluster_profile_agg.to_string())
 
